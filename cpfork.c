011ff7f2ea745b1c0141f8d31804d200d9374b21
saraa93-bit <saraa93177228@gmail.com>
2023-11-15 21:38:21 +0200

    read

--- cpfork.c
+++ cpfork.c
@@ -1,22 +1,25 @@
 #include "main.h"
-int chilp_pid() {
+int chilp_pid(void)
+{
     pid_t chilp;
-
+    
     chilp = fork();
 
-    if (chilp < 0) {
-        printf(stderr, "Fork failed\n");
-        return 1;
-    }
-    else if (chilp == 0)
+    if (chilp < 0)
     {
-        printf("\n");
+        printf(stderr, "Fork failed\n");
+        return (1);
+    }
+     else if (chilp == 0)
+    {
+    printf("\n");
     }
     else
     {
         printf(error);
-    }
 
+        free (chilp);
 
-    return (0);
+        return (0);
+
 }
