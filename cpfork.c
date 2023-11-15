520bc9c22a54d9e959ba64d37d561cb8d2e2d5cb
abdallahgamal110 <abdallahgamal840@gmail.com>
2023-11-15 22:53:19 +0200

    A

--- cpfork.c
+++ cpfork.c
@@ -1,25 +1,29 @@
 #include "main.h"
+/**
+ * chilp_pid - function that creates a child process
+ * Return: 0 if successful, 1 if not
+*/
 int chilp_pid(void)
 {
-    pid_t chilp;
-    
-    chilp = fork();
+	pid_t chilp;
 
-    if (chilp < 0)
-    {
-        printf(stderr, "Fork failed\n");
-        return (1);
-    }
-     else if (chilp == 0)
-    {
-    printf("\n");
-    }
-    else
-    {
-        printf(error);
+	chilp = fork();
 
-        free (chilp);
+	if (chilp < 0)
+	{
+		printf(stderr, "Fork failed\n");
+		return (1);
+	}
+	else if (chilp == 0)
+	{
+	printf("\n");
+	}
+	else
+	{
+		perror("Error");
 
-        return (0);
+		free(chilp);
 
+		return (0);
+	}
 }
