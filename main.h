feb89131e61b3ebd4ebe459d72df15140f26889c
saraa93-bit <saraa93177228@gmail.com>
2023-11-15 20:41:54 +0200

    read

--- main.h
+++ main.h
@@ -12,12 +12,12 @@ extern char **environ;
 
 /* functions prototype */
 
-char* read_line(void);
+char *read_line(void);
 void interactive_shell(void);
 void non_interactive_shell(void);
-char** argv_array(char* line);
-int tokens_number(char* line);
-char* copy_line(char* line);
+char **argv_array(char *line);
+int tokens_number(char *line);
+char *copy_line(char *line);
 char *_strcpy(char *dest, char *src);
 char *_strdup(char *str);
 int _strcmp(char *s1, char *s2);
@@ -25,6 +25,6 @@ int _strlen(char *s);
 char *_strcat(char *dest, char *src);
 void h_exit(char *c);
 void exec_command(char **argv);
-char *_getenv (char *n);
+char *_getenv(char *n);
 
 #endif
