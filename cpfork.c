299144f68c2558b41125fc99453e82caeff44f1e
abdallahgamal110 <abdallahgamal840@gmail.com>
2023-11-15 23:19:25 +0200

    A

--- cpfork.c
+++ cpfork.c
@@ -27,3 +27,18 @@ int chilp_pid(void)
 		return (0);
 	}
 }
+/**
+ * _free - frees the memory
+ * @ptr: pointer to the memory
+ * Return: 1 if successful, 0 if not
+ */
+int _free(void **ptr)
+{
+	if (ptr && *ptr)
+	{
+		free(*ptr);
+		*ptr = NULL;
+		return (1);
+	}
+	return (0);
+}
